91aa3510a2b0347f329c627bb802cb0271e7dc58
Nikhil <njaddu@iu.edu>
2024-07-01 23:54:47 -0400

    Expense update API added

--- controllers/expenseUpdate.js
+++ controllers/expenseUpdate.js
@@ -1,12 +1,13 @@
 import expenseLog from "../models/expensesLog.model.js"
 const expenseUpdate =  async (req,res)=>{
     const expenseID = req.params.email
+    
     const newLog = {
         $push: {
             logs: req.body.logs[0] // Assuming logs is an array sent in the body and you want to add the first element
         }
     }
-
+    
     const sendExpense = await expenseLog.findOneAndUpdate({email:expenseID}, newLog, { new: true, upsert: true, runValidators: true })
 
     if(!sendExpense){
